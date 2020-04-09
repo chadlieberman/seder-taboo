9a7431514dd356ed0de757912cc02c8907562b3e
Chad Lieberman <chad@chadelieberman.com>
2020-04-09 09:56:29 -0400

    more realistic

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ import express from 'express'
 import fs from 'fs'
 import http from 'http'
 import path from 'path'
+import cors from 'cors'
+import DEFAULT_CARDS from './taboo-cards.json'
 const PORT = process.env.PORT || 5000
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+let cards = DEFAULT_CARDS.map((c, idx) => {
+    return Object.assign({}, c, {
+        used: false,
+        id: idx
+    })
+})
 
 let app = express()
 const server = http.Server(app)
@@ -10,9 +27,58 @@ const server = http.Server(app)
 app.set('port', PORT)
 
 app.use('/static', express.static(__dirname + '/build/static'))
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, 'build/index.html'))
+app.post('/api/cards/shuffle', (req, res) => {
+    console.log('POST /api/cards/shuffle')
+    try {
+        cards = shuffle(cards)
+        cards = cards.map((c) => {
+            return Object.assign({}, c, {
+                used: false
+            })
+        })
+        console.log('cards =', cards)
+        res.json({cards})
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({error: err})
+    }
+
+})
+
+app.post('/api/cards/:id', (req, res) => {
+    console.log(`PATCH /api/cards/${req.params.id}`)
+    try {
+        const {used} = req.body
+        if (used !== true) throw("Patches can only be for used true")
+        const {id} = req.params
+        console.log('id =', id)
+        const idx = cards.findIndex((c) => c.id == id)
+        if (idx === -1) throw("Could not find card to update")
+        cards[idx] = Object.assign({}, cards[idx], req.body)
+        console.log(`cards[${idx}] =`, cards[idx])
+        res.json({updated_card: cards[idx]})
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({error: err})
+    }
+})
+
+app.get('/api/cards', (req, res) => {
+    console.log('GET /api/cards')
+    try {
+        const unused_cards = cards.filter((c) => !c.used)
+        res.status(200).json({cards: unused_cards})
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({error: err})
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
 server.listen(PORT, () => {
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,81 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider, connect } from 'react-redux'
-import TABOO_CARDS from './taboo-cards.json'
-console.log(TABOO_CARDS)
+import fetch$ from 'kefir-fetch'
 
+const base_url = window.location.href.includes('localhost')
+    ? 'http://localhost:5000/'
+    : window.location.ref
 
+const API_BASE_URL = base_url + 'api'
 
-class Timer extends React.Component {
+const Card = ({main_word, banned_words}) => (
+    <div id='taboo-card'>
+        <div id='main-word'>
+            {main_word}
+        </div>
+        <div id='banned-words'>
+            <p>Do NOT mention these words:</p>
+            <ul>
+                {banned_words.map((w, i) => (
+                    <li key={i}>{w}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+)
+
+const initial_state = {
+    time: 30,
+    timer_id: null,
+    cards: null,
+    card_idx: null,
+    got: 0,
+    pass: 0,
+    buzz: 0
+}
+
+class Game extends React.Component {
     constructor(props) {
         super(props)
         this.countDown = this.countDown.bind(this)
-        this.state = {
-            time: 30,
-            timer_id: null
-        }
+        this.shuffle = this.shuffle.bind(this)
+        this.start = this.start.bind(this)
+        this.setCards = this.setCards.bind(this)
+        this.gotIt = this.gotIt.bind(this)
+        this.buzz = this.buzz.bind(this)
+        this.pass = this.pass.bind(this)
+        this.markUsed = this.markUsed.bind(this)
+        this.state = initial_state
+    }
+
+    componentDidMount() {
+        fetch$('get', API_BASE_URL + '/cards')
+            .onValue(this.setCards)
+            .onError((resp) => {
+                console.error(resp)
+            })
+    }
+
+    setCards(resp) {
+        console.log('setCards', resp)
+        const {cards} = resp
+        console.log('cards =', cards)
+        this.setState({cards})
+    }
+
+    shuffle() {
+        const {timer_id} = this.state
+        clearTimeout(timer_id)
+        fetch$('post', API_BASE_URL + '/cards/shuffle')
+            .onValue((resp) => {
+                console.log('shuffle response =', resp)
+                this.setState(initial_state)
+                this.setCards(resp)
+            })
+            .onError((resp) => {
+                console.error(resp)
+            })
     }
 
     countDown() {
@@ -27,54 +89,95 @@ class Timer extends React.Component {
         }
     }
 
-    componentDidMount() {
-        const timer_id = setInterval(
-            this.countDown, 1000);
-        this.setState({timer_id})
+    buzz(card_id) {
+        const {buzz} = this.state
+        this.setState({buzz: buzz+1})
+        this.markUsed(card_id)
+    }
+
+    gotIt(card_id) {
+        const {got} = this.state
+        this.setState({got: got + 1})
+        this.markUsed(card_id)
+    }
+
+    pass(card_id) {
+        const {pass} = this.state
+        this.setState({pass: pass + 1})
+        this.markUsed(card_id)
+    }
+
+    markUsed(card_id) {
+        let {cards, card_idx} = this.state
+        cards = cards.map((c, idx) => {
+            if (c.id === card_id) {
+                c.used = true
+            }
+            return c
+        })
+        card_idx = Math.min(cards.length - 1, card_idx + 1)
+        this.setState({
+            cards,
+            card_idx
+        })
+        fetch$('post', API_BASE_URL + `/cards/${card_id}`, {body: {used: true}})
+            .onValue(() => {})
+            .onError((v) => {console.error(v)})
+    }
+
+    start() {
+        const {card_idx} = this.state
+        if (card_idx !== null) return
+        this.setState({
+            card_idx: 0
+        }, () => {
+            const timer_id = setInterval(
+                this.countDown, 1000);
+            this.setState({timer_id})
+        })
     }
 
     render() {
-        const {time} = this.state
-        let text = ''
-        if (time > 0) {
-            text = `${time} seconds remaining`
+        const {time, card_idx, cards, got, pass, buzz} = this.state
+        let timer_text = ''
+        if (time >= 10) {
+            timer_text = `00:${time}`
+        } else if (time > 0) {
+            timer_text = `00:0${time}`
         } else {
-            text = "TIME'S UP!"
+            timer_text = "TIME'S UP!"
         }
 
+        const card = card_idx !== null ? cards[card_idx] : null
+        const num_cards = cards !== null ? cards.filter((c) => !c.used).length : null
         return (
-            <div id='timer'>{text}</div>
-        )
-    }
-
-}
-
-class TabooCard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            card: TABOO_CARDS[Math.floor(Math.random() * TABOO_CARDS.length)]
-        }
-    }
-
-    componentDidMount() {
-        console.log('[TabooCard.ComponentDidMount]')
-    }
-
-    render() {
-        const {main_word, banned_words} = this.state.card
-        return (
-            <div id='taboo-card'>
-                <div id='main-word'>
-                    {main_word}
+            <div>
+                {num_cards !== null && (
+                    <p>There are {num_cards} cards remaining in the deck</p>
+                )}
+                <div id='game'>
+                    <div id='score'>
+                        <span>Got: {got}</span>
+                        <span>Pass+Buzz: {pass + buzz}</span>
+                    </div>
+                    <div id='timer'>{timer_text}</div>
+                    {card !== null && (
+                        <div>
+                            <Card {...card} />
+                            <div id='play-controls'>
+                                <button onClick={() => this.gotIt(card.id)}>Got it!</button>
+                                <button onClick={() => this.buzz(card.id)}>Buzz!</button>
+                                <button onClick={() => this.pass(card.id)}>Pass</button>
+                            </div>
+                        </div>
+                    )}
+                    {card === null && (
+                        <button onClick={this.start}>Start</button>
+                    )}
                 </div>
-                <div id='banned-words'>
-                    <p>Do NOT mention these words:</p>
-                    <ul>
-                        {banned_words.map((w, i) => (
-                            <li key={i}>{w}</li>
-                        ))}
-                    </ul>
+                <div id='shuffling'>
+                    <p>Only click this when the whole game is over... it completely resets everything!</p>
+                    <button onClick={this.shuffle}>Shuffle</button>
                 </div>
             </div>
         )
@@ -91,10 +194,7 @@ class Container extends React.Component {
     render() {
         //console.log('props = ', this.props)
         return (
-            <div>
-                <Timer />
-                <TabooCard />
-            </div>
+            <Game />
         )
     }
 }
